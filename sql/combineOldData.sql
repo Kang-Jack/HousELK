47e67845b9f40b9a9197b686163b47b632270d2e
Kang <Kang-Jack@users.noreply.github.com>
2018-08-02 11:44:04 +0800

    Add files via upload

--- sql/combineOldData.sql
+++ sql/combineOldData.sql
@@ -1,31 +1,69 @@
+ALTER TABLE `house`.`community` 
+RENAME TO  `house`.`communityold` ;
+
+
+ALTER TABLE `house`.`hisprice` 
+RENAME TO  `house`.`hispriceold` ;
+
+
+ALTER TABLE `house`.`houseinfo` 
+RENAME TO  `house`.`houseinfoold` ;
+
+ALTER TABLE `house`.`monthsellinfo` 
+RENAME TO  `house`.`monthsellinfoold` ;
+
+
+ALTER TABLE `house`.`rentinfo` 
+RENAME TO  `house`.`rentinfoold` ;
+
+
+ALTER TABLE `house`.`sellinfo` 
+RENAME TO  `house`.`sellinfoold` ;
+
+
+
+
 INSERT INTO house.community
-	SELECT * FROM house.`community-2` WHERE house.`community-2`.id 
+    SELECT * FROM house.`communityold` WHERE house.`communityold`.id 
     not in 
     (select  house.community.id from house.community) ;
     
 INSERT INTO house.houseinfo
-	SELECT * FROM house.`houseinfo-2` WHERE house.`houseinfo-2`.houseID 
+    SELECT * FROM house.`houseinfoold` WHERE house.`houseinfoold`.houseID 
     not in 
     (select  house.houseinfo.houseID from house.houseinfo) ;
     
 INSERT INTO house.hisprice
-	SELECT * FROM house.`hisprice-2` WHERE house.`hisprice-2`.houseID 
+    SELECT * FROM house.`hispriceold` WHERE house.`hispriceold`.houseID 
     not in 
     (select  house.hisprice.houseID from house.hisprice) ;
     
 INSERT INTO house.rentinfo
-	SELECT * FROM house.`rentinfo-2` WHERE house.`rentinfo-2`.houseID 
+    SELECT * FROM house.`rentinfoold` WHERE house.`rentinfoold`.houseID 
     not in 
     (select  house.rentinfo.houseID from house.rentinfo) ;
     
 INSERT INTO house.sellinfo
-	SELECT * FROM house.`sellinfo-2` WHERE house.`sellinfo-2`.houseID 
+    SELECT * FROM house.`sellinfoold` WHERE house.`sellinfoold`.houseID 
     not in 
     (select  house.sellinfo.houseID from house.sellinfo) ;
+#=============================================================================
+create table house.monthSellinfo  as
+select * from house.sellinfo where house.sellinfo.dealdate like '%30天%';
 
-#=============clean leatest 30 sellinfo==================================================================
-delete
-FROM
+INSERT INTO house.monthSellinfo
+	SELECT * FROM house.`monthSellinfoold` WHERE house.`monthSellinfoold`.houseID 
+    not in 
+    (select  house.monthSellinfo.houseID from house.monthSellinfo) ;
+
+Delete FROM
+    house.sellinfo
+where
+	house.sellinfo.dealdate like '%30天%';
+
+#=============clean leatest 30 sellinfo if price info been updated==================================================================
+
+Delete FROM
     house.monthSellinfo
 where
     house.monthSellinfo.houseID in (select 
@@ -40,4 +78,20 @@ update house.houseinfo set house.houseinfo.isSold = 'yes'
 where house.houseinfo.houseID in (select house.monthSellinfo.houseID from house.monthSellinfo);
 
 update house.houseinfo set house.houseinfo.isSold = 'yes'
-where house.houseinfo.houseID in (select house.sellinfo.houseID from house.sellinfo);sellinfo
+where house.houseinfo.houseID in (select house.sellinfo.houseID from house.sellinfo);
+
+
+
+#=================delete old=====================================================================
+DROP TABLE `house`.`communityold`;
+DROP TABLE `house`.`hispriceold`;
+DROP TABLE `house`.`houseinfoold`;
+DROP TABLE `house`.`monthsellinfoold`;
+DROP TABLE `house`.`rentinfoold`;
+DROP TABLE `house`.`sellinfoold`;
+
+
+
+
+
+
